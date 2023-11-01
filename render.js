c10548da51f7e5ee370afd961495147cb7c12036
Reinaldo Magalhaes <reinaldomagalhaes96@gmail.com>
2023-11-01 12:37:02 -0500

    improve styling

--- render.js
+++ render.js
@@ -8,80 +8,129 @@ export class Render {
             width: width,
             height: height,
         });
+
+        this.layer = null;
+    }
+
+    drawBackground() {
+        const rectOuter = new Konva.Rect({
+            x: 0,
+            y: 0,
+            width: this.width,
+            height: this.height,
+            fill: '#FFF',
+            stroke: 'black',
+            strokeWidth: 1,
+        });
+
+        const rect = new Konva.Rect({
+            x: 20,
+            y: 20,
+            width: this.width - 40,
+            height: this.height - 40,
+            fill: '#E0E0E0',
+            stroke: 'black',
+            strokeWidth: 1,
+        });
+
+        this.layer.add(rectOuter);
+        this.layer.add(rect);
     }
 
     drawGraph(graph, path = [], startId, targetId) {
 
         // reset layer
         this.stage.destroyChildren();
-        const layer = new Konva.Layer();
-        this.stage.add(layer);
+        this.layer = new Konva.Layer();
+        this.stage.add(this.layer);
+        this.drawBackground();
 
         const lines = {};
 
         graph.edges.forEach(edge => {
             lines[`${edge.nodeA.id}-${edge.nodeB.id}`] = new Konva.Line({
                 points: [edge.nodeA.point.x, edge.nodeA.point.y, edge.nodeB.point.x, edge.nodeB.point.y],
-                stroke: graph.isEdgeInPath(edge, path) ? "blue" : "gray",
-                strokeWidth: 15,
+                stroke: graph.isEdgeInPath(edge, path) ? "blue" : "#FFFFFF",
+                strokeWidth: 5,
                 lineCap: 'round',
                 lineJoin: 'round'
             });
-            layer.add(lines[`${edge.nodeA.id}-${edge.nodeB.id}`]);
+            this.layer.add(lines[`${edge.nodeA.id}-${edge.nodeB.id}`]);
         });
 
-        Object.keys(graph.nodes).forEach(key => {
-            const node = graph.nodes[key];
-            const nodeX = node.point.x;
-            const nodeY = node.point.y;
+        Object.keys(graph.nodes)
+            .filter(key => graph.nodes[key].id === startId || graph.nodes[key].id === targetId)
+            .forEach(key => {
+                const node = graph.nodes[key];
+                const nodeX = node.point.x;
+                const nodeY = node.point.y;
 
-            const group = new Konva.Group({
-                x: nodeX,
-                y: nodeY,
-                draggable: true
-            });
-    
-            const circle = new Konva.Circle({
-                x: 0,
-                y: 0,
-                radius: 10,
-                fill: node.id === startId ? "lightblue" : node.id === targetId ? "blue" : "orange",
-                stroke: 'black',
-                strokeWidth: 2,
-            });
+                const group = new Konva.Group({
+                    x: nodeX,
+                    y: nodeY,
+                    draggable: true
+                });
 
-            const nodeIdText = new Konva.Text({
-                x: -10,
-                y: -9,
-                text: node.id,
-                fontSize: 18,
-                fontFamily: 'Calibri',
-                fill: '#555',
-                width: 20,
-                padding: 0,
-                align: 'center',
-            });
+                const line = new Konva.Line({
+                    points: [0, 0, 0, -20],
+                    stroke: "black",
+                    strokeWidth: 2,
+                    lineCap: 'round',
+                    lineJoin: 'round'
+                });
 
-            group.add(circle);
-            group.add(nodeIdText);
-            layer.add(group);
+                const circleSmall = new Konva.Circle({
+                    x: 0,
+                    y: 0,
+                    radius: 2,
+                    fill: "black",
+                    stroke: 'black',
+                    strokeWidth: 1,
+                });
 
-            group.on("dragmove", () => {
-                node.point = { x: group.attrs.x, y: group.attrs.y };
-                graph.edges.forEach(edge => {
-                    if (edge.nodeA.id === node.id) {
-                        edge.nodeA.point = { x: group.attrs.x, y: group.attrs.y };
-                        lines[`${edge.nodeA.id}-${edge.nodeB.id}`].attrs.points[0] = group.attrs.x;
-                        lines[`${edge.nodeA.id}-${edge.nodeB.id}`].attrs.points[1] = group.attrs.y;
-                    }
-                    else if (edge.nodeB.id === node.id) {
-                        edge.nodeB.point = { x: group.attrs.x, y: group.attrs.y };
-                        lines[`${edge.nodeA.id}-${edge.nodeB.id}`].attrs.points[2] = group.attrs.x;
-                        lines[`${edge.nodeA.id}-${edge.nodeB.id}`].attrs.points[3] = group.attrs.y;
-                    }
-                })
+                const circle = new Konva.Circle({
+                    x: 0,
+                    y: -20,
+                    radius: 10,
+                    fill: node.id === startId ? "#9E9E9E" : node.id === targetId ? "red" : "orange",
+                    stroke: 'black',
+                    strokeWidth: 1,
+                });
+
+                const nodeIdText = new Konva.Text({
+                    x: -10,
+                    y: -29,
+                    text: node.id === startId ? "A" : node.id === targetId ? "B" : "N",
+                    fontSize: 18,
+                    fontFamily: 'Calibri',
+                    fill: '#FFF',
+                    width: 20,
+                    padding: 0,
+                    align: 'center',
+                });
+
+                group.add(line);
+                group.add(circleSmall);
+                group.add(circle);
+                group.add(nodeIdText);
+                this.layer.add(group);
+
+                group.on("dragmove", () => {
+                    node.point = { x: group.attrs.x, y: group.attrs.y };
+                    graph.edges.forEach(edge => {
+                        if (edge.nodeA.id === node.id) {
+                            edge.nodeA.point = { x: group.attrs.x, y: group.attrs.y };
+                            lines[`${edge.nodeA.id}-${edge.nodeB.id}`].attrs.points[0] = group.attrs.x;
+                            lines[`${edge.nodeA.id}-${edge.nodeB.id}`].attrs.points[1] = group.attrs.y;
+                        }
+                        else if (edge.nodeB.id === node.id) {
+                            edge.nodeB.point = { x: group.attrs.x, y: group.attrs.y };
+                            lines[`${edge.nodeA.id}-${edge.nodeB.id}`].attrs.points[2] = group.attrs.x;
+                            lines[`${edge.nodeA.id}-${edge.nodeB.id}`].attrs.points[3] = group.attrs.y;
+                        }
+                    })
+                });
+
             });
-    
-        });
     }
 }
